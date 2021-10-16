782e6109eccf239357af6423f967a4bdae17b925
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-10-15 21:01:59 -0400

    Adds get request for /friends and post request for /secret

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,21 @@ const courageArray = [
   "./images/courage-scared.png",
   "./images/courage.jpg",
 ];
+const img = document.querySelectorAll('img');
+const galleryImageSecondary = document.querySelectorAll('.gallery__image--secondary');
+
 sectionButton.addEventListener("click", (e) => {
   e.preventDefault();
   const randomNum = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
   sectionImage.setAttribute("src", `${courageArray[randomNum]}`);
 });
+
+// can't get this working
+for (let i = 0; i < img.length; i++) {
+  img.addEventListener("click", (e) => {
+    e.preventDefault();
+    const p = document.createElement('p');
+    p.innerText = 'hello world';
+    galleryImageSecondary[i].append(p);
+  });
+}
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/heartbeat', (req, res) => {
   res.json({
     'is': 'working'
@@ -23,12 +25,32 @@ server.set('view engine', 'html');
 
 server.get('/', (req, res) => {
   res.render('landing', {
+    locals: {
+      title: 'Hello World'
+    },
     partials: {
       footer: 'partials/footer',
       header: 'partials/header'
     }
   })
 });
+
+server.get('/friends', (req, res) => {
+  res.render('friends', {
+    locals: { 
+      friends: ['Anna', 'Billy', 'Carlos', 'David', 'Eunice']
+    }
+  });
+});
+
+// query params
+server.post('/secret', (req, res) => {
+  const randomNum = Math.floor(Math.random() * 10) + 1
+  const { secret } = req.body;
+  if (randomNum == secret) {
+    res.send(`You guessed correctly!`);
+  } else res.send(`You guessed incorrectly`)
+})
 
 const { PORT } = process.env;
 
